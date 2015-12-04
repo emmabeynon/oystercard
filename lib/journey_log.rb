3e52208240c853c2302e6177682c23810ed60046
emmabeynon <emma.beynon@gmail.com>
2015-12-04 11:50:34 +0000

    Amended Journey initialize to accept journey_klass dependency injection. start_journey now creates a new Journey class that accepts an entry station. current_journey now returns the journey.

--- lib/journey_log.rb
+++ lib/journey_log.rb
@@ -1,9 +1,19 @@
 require_relative 'journey'
 
 class JourneyLog
-  attr_reader :journey
+  attr_reader :journey, :journey_klass
+
+  def initialize(journey_klass)
+    @journey_klass = journey_klass
+  end
 
   def start_journey(entry_station)
-    @journey = Journey.new(entry_station)
+    @journey = journey_klass.new(entry_station)
+  end
+
+  # private
+
+  def current_journey
+    journey
   end
 end
--- spec/journey_log_spec.rb
+++ spec/journey_log_spec.rb
@@ -1,9 +1,16 @@
 require 'journey_log'
 
 describe JourneyLog do
-  subject(:journey_log) { described_class.new }
+  subject(:journey_log) { described_class.new(:journey_klass) }
   let(:journey) { double :journey, :entry_station => station }
   let(:entry_station) { double :station}
+
+  describe '#default' do
+    # it 'initializes with a journey_klass' do
+      it { is_expected.to respond_to :journey_klass }
+    # end
+  end
+
   describe '#start_journey' do
     it 'should start a new journey with an entry station' do
       journey_log.start_journey(entry_station)
